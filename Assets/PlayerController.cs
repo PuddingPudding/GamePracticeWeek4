ccac457a96181f0282f7823ba30a98f0a2ccb2e2
PuddingPudding <hwsh70119@gmail.com>
2017-03-09 21:28:19 +0800

    更改地圖的碰撞判定，部分物景現在有碰撞器了(你無法再穿過他)
    更改主角的移動方式，使用WASD四鍵進行移動之方向判定

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,6 +10,10 @@ public class PlayerController : MonoBehaviour
     public Transform rotateXTransform;
     public float rotateSpeed;
     public float currentRotateX = 0;
+    float currentSpeed = 0;
+    public float moveSpeed;
+
+    public Rigidbody rigidBody;
 
     // Use this for initialization
     void Start()
@@ -17,22 +21,44 @@ public class PlayerController : MonoBehaviour
         animatorController = this.GetComponent<Animator>();
     }
 
-    public float MoveSpeed;
-    float currentSpeed = 0;
-
     // Update is called once per frame
     void Update()
     {
-        float result = 0;
-        if (Input.GetKey(KeyCode.W))
-        {
-            result += MoveSpeed;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            result -= MoveSpeed;
-        }
+        //決定鍵盤input的結果
+        Vector3 movDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) { movDirection.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movDirection.z -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movDirection.x += 1; }
+        if (Input.GetKey(KeyCode.A)) { movDirection.x -= 1; }
+        movDirection = movDirection.normalized;
 
+        //決定要給Animator的動畫參數
+        if (movDirection.magnitude == 0)
+        {
+            currentSpeed = 0;
+        }
+        else
+        {
+            if (movDirection.z < 0)
+            {
+                currentSpeed = -moveSpeed;
+            }
+            else
+            {
+                currentSpeed = moveSpeed;
+            }
+        }
+        animatorController.SetFloat("Speed", currentSpeed);
+
+        //轉換成世界座標的方向
+        Vector3 worldSpaceDirection = movDirection.z * rotateYTransform.transform.forward +
+                                      movDirection.x * rotateYTransform.transform.right;
+        Vector3 velocity = rigidBody.velocity;
+        velocity.x = worldSpaceDirection.x * moveSpeed;
+        velocity.z = worldSpaceDirection.z * moveSpeed;
+        rigidBody.velocity = velocity;
+
+        //計算滑鼠角度
         rotateYTransform.transform.localEulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0) * rotateSpeed;
         currentRotateX += Input.GetAxis("Vertical") * rotateSpeed;
         if (currentRotateX > 90)
@@ -44,10 +70,5 @@ public class PlayerController : MonoBehaviour
             currentRotateX = -90;
         }
         rotateXTransform.transform.localEulerAngles = new Vector3(-currentRotateX, 0, 0);
-        currentSpeed = result;
-
-        rotateYTransform.transform.position += Time.deltaTime * currentSpeed * rotateYTransform.transform.forward;
-        //this.transform.position += Time.deltaTime * currentSpeed * this.transform.forward;
-        animatorController.SetFloat("Speed", currentSpeed);
     }
 }
